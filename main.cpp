69fe9a246ee32c8f216d014b13c442345492724f
mshimada <mshimada2@mail.csuchico.edu>
2018-03-01 11:07:58 -0800

    please work

--- main.cpp
+++ main.cpp
@@ -5,7 +5,34 @@ using namespace std;
 
 int main()
 {
-  SecretDoor game;
-    
-  return 0;
+        SecretDoor game;
+
+        int attempts;
+        int wins;
+
+        cout<<"How many times do you want to play the game? "<<endl;
+        cin>>attempts;
+
+        int index = attempts;
+
+        for(index = 0; index < attempts; index++){
+
+               game.newGame();
+
+               game.guessDoorC();
+
+               game.guessDoorC();
+
+
+        }
+
+    if(game.isWinner() == true){
+
+    wins++;
+
+    cout<<wins<<endl;
+
+    }
+
+return 0;
 }
